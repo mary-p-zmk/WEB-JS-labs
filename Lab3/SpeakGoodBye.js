4c5efd3c3b2bc1e840958f065dc1cd34220cacb1
mary-p-zmk <146123741+mary-p-zmk@users.noreply.github.com>
2026-03-20 00:22:53 +0200

    Update SpeakGoodBye.js

--- Lab3/SpeakGoodBye.js
+++ Lab3/SpeakGoodBye.js
@@ -1,6 +1,10 @@
-var speakWord = "Good Bye";
+(function (window) {
+  var byeSpeaker = {};
+  var speakWord = "Good Bye"; 
 
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
 
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+  window.byeSpeaker = byeSpeaker;
+})(window);
