8306e224a1512e28d1fab33a5e52e65b3252cbef
mary-p-zmk <146123741+mary-p-zmk@users.noreply.github.com>
2026-03-20 00:25:15 +0200

    Update script.js

--- Lab3/script.js
+++ Lab3/script.js
@@ -1,11 +1,25 @@
-var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+(function () {
+  var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
-for () {
+  console.log("--- Standard Logic (Task 1.2.2) ---");
+  for (var i = 0; i < names.length; i++) {
+    
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]); 
+    }
+  }
 
   
-  if () {
-    
-  } else {
-    
+  console.log("\n--- Custom Logic: Selection by name length (Task 1.2.3) ---");
+  console.log("Annotation: Names with more than 4 letters get a special mention.");
+  
+  for (var i = 0; i < names.length; i++) {
+    if (names[i].length > 4) {
+      console.log("Long name detected: " + names[i]);
+    }
   }
-}
+})();
