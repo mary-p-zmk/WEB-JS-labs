46df9da52542f4d07f4e3177768c9599abbee776
mary-p-zmk <146123741+mary-p-zmk@users.noreply.github.com>
2026-04-06 00:58:51 +0300

    Update main.js

--- Lab4/main.js
+++ Lab4/main.js
@@ -6,11 +6,11 @@ function generateArray(size) {
 const baseArr = generateArray(100);
 
 console.log("%c ТЕСТ 1: Нерозріджений масив (100 елементів) ", "background: #222; color: #bada55");
-MySortLib.bubbleSort([...baseArr]);
-MySortLib.selectionSort([...baseArr]);
-MySortLib.insertionSort([...baseArr]);
-MySortLib.shellSort([...baseArr]);
-MySortLib.quickSort([...baseArr]);
+SortLib.bubbleSort([...baseArr]);
+SortLib.selectionSort([...baseArr]);
+SortLib.insertionSort([...baseArr]);
+SortLib.shellSort([...baseArr]);
+SortLib.quickSort([...baseArr]);
 
 console.log("%c ТЕСТ 2: Розріджений масив (100 елементів) ", "background: #222; color: #ffcc00");
 
